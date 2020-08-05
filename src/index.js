93ecde1a7ced980cf7f6887e6a166bf58bdad5ba
Afonso Machado <afonso777@hotmail.com>
2020-08-04 23:30:20 -0300

    fundo do app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 
 import '~/config/ReactotronConfig';
 
 import Routes from '~/routes';
 
-const App = () => <Routes />;
+const App = () => (
+  <>
+    {/* definindo cor padrão da status bar */}
+    <StatusBar barStyle="light-content" backgroundColor="#8b10ae" />
+    <Routes />
+  </>
+);
 
 export default App;
