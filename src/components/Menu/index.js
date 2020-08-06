30279bf857e4e912a23c2fd051cbed4b2f09e955
Afonso Machado <afonso777@hotmail.com>
2020-08-05 23:40:30 -0300

    campo de seleção do menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import QRCode from 'react-native-qrcode-svg';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Container, Code} from './styles';
+import {Container, Code, Nav, NavItem, NavText} from './styles';
 
 export default function Menu() {
   return (
@@ -14,6 +15,29 @@ export default function Menu() {
           color="#8b10ae"
         />
       </Code>
+
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#fff" />
+          <NavText>Me ajuda</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#fff" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="attach-money" size={20} color="#fff" />
+          <NavText>Configurar conta</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#fff" />
+          <NavText>Configurar cartão</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#fff" />
+          <NavText>Configurações do app</NavText>
+        </NavItem>
+      </Nav>
     </Container>
   );
 }
--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,12 +1,34 @@
+import {StyleSheet} from 'react-native';
 import styled from 'styled-components/native';
 
-export const Container = styled.ScrollView.attrs({
-  contentContainerStyle: {alignItems: 'center'},
-})`
+export const Container = styled.ScrollView`
   margin: 0 30px;
 `;
 
 export const Code = styled.View`
   background: #fff;
   padding: 5px;
+  align-self: center;
+`;
+
+export const Nav = styled.View`
+  width: 100%;
+  margin-top: 30px;
+  /* menor que 1px */
+  border-top-width: ${StyleSheet.hairlineWidth}px;
+  border-top-color: rgba(255, 255, 255, 0.8);
+`;
+
+export const NavItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 12px 0;
+  border-bottom-width: ${StyleSheet.hairlineWidth}px;
+  border-bottom-color: rgba(255, 255, 255, 0.8);
+`;
+
+export const NavText = styled.Text`
+  font-size: 15px;
+  color: #fff;
+  margin-left: 20px;
 `;
